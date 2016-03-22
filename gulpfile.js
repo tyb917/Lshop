f6c16a722d41c826009d4523010ac5eeafda2c29
tan <461670819@qq.com>
2016-03-22 09:28:55 +0800

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,42 @@ elixir(function (mix) {
     mix
         .phpUnit()
 
-    /**
-     * Copy needed files from /node directories
-     * to /public directory.
-     */
+        /**
+         * Copy needed files from /node directories
+         * to /public directory.
+         */
         .copy(
-        'node_modules/font-awesome/fonts',
-        'public/build/fonts/font-awesome'
-    )
+            'node_modules/font-awesome/fonts',
+            'public/build/fonts/font-awesome'
+        )
         .copy(
-        'node_modules/bootstrap-sass/assets/fonts/bootstrap',
-        'public/build/fonts/bootstrap'
-    )
+            'node_modules/bootstrap-sass/assets/fonts/bootstrap',
+            'public/build/fonts/bootstrap'
+        )
         .copy(
-        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-        'public/js/vendor/bootstrap'
-    )
+            'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+            'public/js/vendor/bootstrap'
+        )
 
-    /**
-     * Process frontend SCSS stylesheets
-     */
+        /**
+         * Process frontend SCSS stylesheets
+         */
         .sass([
             'frontend/app.scss',
             'plugin/toastr/toastr.scss',
             'plugin/sweetalert/sweetalert.scss'
         ], 'resources/assets/css/frontend/app.css')
 
-    /**
-     * Combine pre-processed frontend CSS files
-     */
+        /**
+         * Combine pre-processed frontend CSS files
+         */
         .styles([
             'frontend/app.css',
         ], 'public/css/frontend.css')
 
-    /**
-     * Combine frontend scripts
-     */
+        /**
+         * Combine frontend scripts
+         */
         .scripts([
             'plugins.js',
             'plugin/toastr/toastr.min.js',
@@ -47,9 +47,9 @@ elixir(function (mix) {
             'frontend/app.js'
         ], 'public/js/frontend.js')
 
-    /**
-     * Process member SCSS stylesheets
-     */
+        /**
+         * Process member SCSS stylesheets
+         */
         .sass([
             'member/app.scss',
             'plugin/icheck/all.scss',
@@ -59,16 +59,16 @@ elixir(function (mix) {
             'plugin/datapicker/datepicker3.scss'
         ], 'resources/assets/css/member/app.css')
 
-    /**
-     * Combine pre-processed member CSS files
-     */
+        /**
+         * Combine pre-processed member CSS files
+         */
         .styles([
             'member/app.css',
         ], 'public/css/member.css')
 
-    /**
-     * Combine member scripts
-     */
+        /**
+         * Combine member scripts
+         */
         .scripts([
             'plugins.js',
             'plugin/toastr/toastr.min.js',
@@ -76,9 +76,9 @@ elixir(function (mix) {
             'member/app.js',
         ], 'public/js/member.js')
 
-    /**
-     * Process backend SCSS stylesheets
-     */
+        /**
+         * Process backend SCSS stylesheets
+         */
         .sass([
             'backend/app.scss',
             'plugin/icheck/all.scss',
@@ -88,30 +88,31 @@ elixir(function (mix) {
             'plugin/datapicker/datepicker3.scss'
         ], 'resources/assets/css/backend/app.css')
 
-    /**
-     * Combine pre-processed backend CSS files
-     */str.min.js',
+        /**
+         * Combine pre-processed backend CSS files
+         */
+        .styles([
+            'backend/app.css',
+        ], 'public/css/backend.css')
+
+        /**
+         * Combine backend scripts
+         */
+        .scripts([
+            'plugins.js',
+            'plugin/icheck/icheck.min.js',
+            'plugin/toastr/toastr.min.js',
             'plugin/select2/select2.min.js',
             'plugin/sweetalert/sweetalert.min.js',
             'plugin/datatables/jquery.dataTables.min.js',
-            'plugi
-                .styles([
-                    'backend/app.css',
-                ], 'public/css/backend.css')
-
-                /**
-                 * Combine backend scripts
-                 */
-                .scripts([
-                    'plugins.js',
-                    'plugin/icheck/icheck.min.js',
-                    'plugin/toastr/toan/datapicker/bootstrap-datepicker.min.js',
+            'plugin/datapicker/bootstrap-datepicker.min.js',
             'plugin/datapicker/bootstrap-datepicker.zh-CN.min.js',
             'backend/app.js',
             'backend/custom.js'
         ], 'public/js/backend.js')
 
-    /**DFcontrol
-     */
+        /**
+         * Apply version control
+         */
         .version(["public/css/frontend.css", "public/js/frontend.js", "public/css/member.css", "public/js/member.js", "public/css/backend.css", "public/js/backend.js"]);
 });
