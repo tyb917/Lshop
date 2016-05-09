7c7604a4171fba5be7e6310cf0aa2e9b955c0ed4
tan <461670819@qq.com>
2016-05-09 19:01:57 +0800

    create store store_group store_website

---
+++ database/migrations/2016_05_09_095505_create_store_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('store', function (Blueprint $table) {
+            $table->smallIncrements('store_id')->comment('商店ID');
+            $table->char('code',30)->nullable()->default(null)->comment('代码');
+            $table->unsignedsmallInteger('website_id')->default(0)->comment('网站ID');
+            $table->foreign('website_id')->references('website_id')->on('store_website')->onDelete('cascade')->onUpdate('restrict');
+            $table->unsignedsmallInteger('group_id')->default(0)->comment('分组ID');
+            $table->foreign('group_id')->references('group_id')->on('store_group')->onDelete('cascade')->onUpdate('restrict');
+            $table->char('name')->comment('名称');
+            $table->unsignedSmallInteger('sort_order')->default(0)->comment('商店排序');
+            $table->unsignedSmallInteger('is_active')->default(0)->comment('商店激活');
+            $table->unique(['is_active','sort_order']);
+            $table->index(['code','website_id','group_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('store', function (Blueprint $table) {
+            //
+        });
+    }
+}
